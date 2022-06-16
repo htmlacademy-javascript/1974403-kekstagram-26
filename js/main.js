9142befd0b5f49055557f351e3d36cf837eb5d4b
Pico <lukasedoy@gmail.com>
2022-06-17 01:04:15 +0300

    Вспомогательные функции 2 проверка

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-function randomInteger(min, max) {
-  const rand = min + Math.random() * (max + 1 - min);
-  return Math.round(rand);
-}
+const randomInteger = (min, max) => {
+  if (min >= 0 && min < max) {
+    return Math.round(min + Math.random() * (max - min) + min);
+  }
+  if (min < 0 || max < 0) {
+    return "Переданные числа не соответствуют условиям";
+  }
+  if (min >= max) {
+    return min;
+  }
+};
 
-randomInteger(0, 10);
+randomInteger(0, 3);
+
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+checkStringLength("Переданные числа не соответствуют условиям", 50);
